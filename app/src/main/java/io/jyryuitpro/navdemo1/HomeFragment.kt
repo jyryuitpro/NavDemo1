02606458e2a4955328338792687d24fc093e956c
jyr <jyr@wavers.co.kr>
2022-09-04 13:34:59 +0900

    Navigation Actions

--- app/src/main/java/io/jyryuitpro/navdemo1/HomeFragment.kt
+++ app/src/main/java/io/jyryuitpro/navdemo1/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import io.jyryuitpro.navdemo1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,6 +20,9 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_home, container, false)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondeFragment)
+        }
         return binding.root
     }
 }
